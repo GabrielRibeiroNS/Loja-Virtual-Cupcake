a400196e7f478863a6615264afd29ba05657567c
Gabriel Ribeiro Nicácio Silva <gabrielribeironi7@gmail.com>
2024-10-17 22:26:01 -0300

    Update index.php

--- cart/index.php
+++ cart/index.php
@@ -22,7 +22,7 @@ require('./sheep_core/config.php');
             <p><i class="fa fa-shopping-cart"></i>0</p>
         </div>
     </div>
-    <!--- dFimd do Topo Sited --->
+    <!--- dFimd do Topo Sitedasd --->
 
 </body>
 </html>
