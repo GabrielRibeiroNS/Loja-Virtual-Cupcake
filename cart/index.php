37bb25c59a5fba5d6c16ec608a23f1f0f0828e6d
Gabriel Ribeiro Nicácio Silva <gabrielribeironi7@gmail.com>
2024-10-24 10:16:34 -0300

    3

--- cart/index.php
+++ cart/index.php
@@ -5,6 +5,7 @@ require('./sheep_core/config.php');
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,8 +13,9 @@ require('./sheep_core/config.php');
     <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
-    
+
     <!--- Topo do Site --->
     <div class="header">
         <p class="logo">Loja Cupcake</p>
@@ -24,5 +26,27 @@ require('./sheep_core/config.php');
     </div>
     <!--- Fim do Topo Site --->
 
+    <!-- Conteudo do Site -->
+    <div class="container">
+        <!-- Linha Produto Site -->
+        <div class="linha-produtos">
+        <!-- Inicio Produto -->
+            <div class="corpoProduto">
+                <div class="imgProduto">
+                    <img src="assets/img/produto-1.jpg" alt="" class="produtoMiniatura">
+                    <div class="titulo">
+                        <p>Curso PHP</p>
+                        <h2>R$ 10,00</h2>
+                    </div>
+                </div>
+            </div>
+        <!-- Fim Produto -->
+        </div>
+        <!-- Fim Linha Produto Site -->
+    </div>
+    <!-- Fim Conteudo do Site -->
+
+
 </body>
+
 </html>
