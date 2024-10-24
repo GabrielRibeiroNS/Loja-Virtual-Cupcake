bcb2262a32e804f737ad989c4828c688a609b6a3
Gabriel Ribeiro Nicácio Silva <gabrielribeironi7@gmail.com>
2024-10-24 09:39:50 -0300

    atualization

--- cart/index.php
+++ cart/index.php
@@ -9,7 +9,7 @@ require('./sheep_core/config.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>vendacupcake.com.br - Gabriel Ribeiro</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
@@ -19,9 +19,10 @@ require('./sheep_core/config.php');
         <p class="logo">Loja Cupcake</p>
         <div class="cart">
             <!--- Logo Carrinho de Compras Font Awesome --->
-            <p><i class="fa fa-shopping-cart"></i>0</p>
+            <p><i class="fa fa-shopping-cart"></i> 0 </p>
         </div>
     </div>
+
     <!--- Fim do Topo Site --->
 
 </body>
