c0fc72765e7de5f68dffe504b648cb760f1f75e7
Gabriel Ribeiro Nicácio Silva <gabrielribeironi7@gmail.com>
2024-11-05 08:11:19 -0300

    2

--- cart/painel/filtros/criar.php
+++ cart/painel/filtros/criar.php
@@ -6,8 +6,17 @@ require('../../sheep_core/config.php');
 $produto = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 if(isset($produto['criarProduto'])){
     unset($produto['criarProduto']);
+    $produto['capa'] = ($_FILES['capa']['tmp_name'] ? $_FILES['capa'] : null); //Se a capa existir no arquivo temporario, o mesmo recebe a foto / Caso contrário ele recebe null = nada
 
-    var_dump($produto);
+    $salvar = new Produtos();
+    $salvar->CriarProduto($produto);
+
+    if($salvar->getResultado()){
+        header("location: ".HOME."/painel/index.php?sucesso=true")
+    }
+
+    //var_dump($produto); MOSTRA RECEBENDO OS ARQUIVOS E SUAS ESPECIFICAÇÔES
+
 }
 
 
