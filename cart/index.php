023c1c8b94126d2105dc3980c77177758faf94d2
Gabriel Ribeiro Nicácio Silva <gabrielribeironi7@gmail.com>
2025-01-12 22:03:59 -0300

    Update index.php

--- cart/index.php
+++ cart/index.php
@@ -156,7 +156,7 @@ require('./sheep_core/config.php');
         <!--Working on CS50 course-->
                 <!--Second Day on CS50 course Horwship Christ-->
                 <!--Today i've learned Scratch from CS50 -->
-        <!--Week 1 C language asd -->
+        <!--Week 1 C language asd HOly-->
 
 
 </html>
